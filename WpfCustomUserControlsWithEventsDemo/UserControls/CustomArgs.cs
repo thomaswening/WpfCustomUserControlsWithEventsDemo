6461e6cd262fd06fa7ed15fffb7d1c2a647d6c10
thomaswening <thomaswening94@gmail.com>
2022-09-01 20:26:19 +0200

    Add project files.

---
+++ WpfCustomUserControlsWithEventsDemo/UserControls/CustomArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCustomUserControlsWithEventsDemo.UserControls
+{
+    internal class CustomArgs
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+
+        public CustomArgs(string name, string age)
+        {
+            Name = name;
+            Age = age;
+        }   
+    }
+}
---
+++ WpfCustomUserControlsWithEventsDemo/UserControls/GreetingUC.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomUserControlsWithEventsDemo.UserControls
+{
+    /// <summary>
+    /// Interaction logic for GreetingUC.xaml
+    /// </summary>
+    public partial class GreetingUC : UserControl
+    { 
+        public GreetingUC()
+        {
+            InitializeComponent();
+        }
+
+        public void MakeGreeting(string pName, string pAge)
+        {
+            if (string.IsNullOrEmpty(pName)) pName = "_____";
+            if (string.IsNullOrEmpty(pAge)) pAge = "__";
+
+            txtGreeting.Text = $"Hello {pName}! You are {pAge} years old.";
+        }
+    }
+}
